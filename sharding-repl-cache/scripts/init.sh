cbe7ee1a8b7306370da6ee7c1b9ae19c5da94613
Murat Baysangurov <b.mourat@gmail.com>
2025-01-09 23:22:28 +0300

    Задание 4

---
+++ sharding-repl-cache/scripts/init.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+echo "Setup configuration server"
+docker compose exec -T configSrv mongosh --port 27017 --quiet <<'EOF'
+rs.initiate(
+{
+    _id : "config_server",
+    configsvr: true,
+    members: [
+    { _id : 0, host : "configSrv:27017" }
+    ]
+}
+);
+exit();
+EOF
+
+echo "Setup shard 1"
+docker compose exec -T shard1_1 mongosh --port 27018 --quiet <<EOF
+rs.initiate(
+    {
+    _id : "shard1",
+    members: [
+        { _id : 0, host : "shard1_1:27018" },
+        { _id : 1, host : "shard1_2:27019" },
+        { _id : 2, host : "shard1_3:27020" },
+    ]
+    }
+);
+exit();
+EOF
+
+echo "Setup shard 2"
+docker compose exec -T shard2_1 mongosh --port 27021 --quiet <<EOF
+rs.initiate(
+    {
+    _id : "shard2",
+    members: [
+        { _id : 0, host : "shard2_1:27021" },
+        { _id : 1, host : "shard2_2:27022" },
+        { _id : 2, host : "shard2_3:27023" },
+    ]
+    }
+);
+exit();
+EOF
+
+echo "Setup router"
+docker compose exec -T mongos_router mongosh --port 27024 --quiet <<EOF
+sh.addShard( "shard1/shard1_1:27018");
+sh.addShard( "shard2/shard2_1:27021");
+
+sh.enableSharding("somedb");
+sh.shardCollection("somedb.helloDoc", { "name" : "hashed" } )
+
+use somedb
+
+for(var i = 0; i < 1000; i++) db.helloDoc.insertOne({age:i, name:"ly"+i})
+
+db.helloDoc.countDocuments() 
+exit();
+EOF 
